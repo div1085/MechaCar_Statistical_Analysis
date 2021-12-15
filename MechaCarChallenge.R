9c24d4619afbb1446d60c803d7e9f16421ccc711
div1085 <89940688+div1085@users.noreply.github.com>
2021-12-15 05:42:18 -0500

    file update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,10 +7,49 @@ library(datasets)  # Load/unload base packages manually
 
 # Deliverable 1: Linear Regression to Predict MPG 
 
-mecha_mpg <- read.csv('MechaCar_mpg.csv') #import dataset
+mecha_mpg <- read.csv('Resources/MechaCar_mpg.csv') #import dataset
 class(mecha_mpg)
 head(mecha_mpg)
 
 mecha_reg <- lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_mpg) #generate multiple linear regression model
 mecha_reg # variable holding multiple reg for mecha car
 summary(mecha_reg) #generate summary statistics
+
+## Deliverable 2: Create Visualizations for the Trip Analysis
+
+mecha_SC <- read.csv('Resources/Suspension_Coil.csv') #import dataset
+
+head(mecha_SC)
+# get the mean, median, variance, and standard deviation of the suspension coil’s PSI column
+
+Mean = mean(mecha_SC$PSI)
+Median=median(mecha_SC$PSI)
+Variance=var(mecha_SC$PSI)
+SD = sd(mecha_SC$PSI)
+
+total_summary <- data.frame(Mean,Median,Variance,SD)
+total_summary
+
+## Write an RScript that creates a lot_summary
+
+lot_summary <-mecha_SC %>% group_by(Manufacturing_Lot) %>% summarise(
+  Mean=mean(PSI),
+  Median = median(PSI),
+  Variance = var(PSI),
+  SD = sd(PSI), 
+  .groups = 'keep')
+lot_summary
+
+## Deliverable 3: T-Tests on Suspension Coils
+
+t.test(mecha_SC$PSI,mu=1500) #compare sample versus population means
+
+t.test(subset(mecha_SC,Manufacturing_Lot=='Lot1')$PSI ,mu=1500) #compare means of lot1 with samples
+
+t.test(subset(mecha_SC,Manufacturing_Lot=='Lot2')$PSI ,mu=1500) #compare means of lot2 with samples
+
+t.test(subset(mecha_SC,Manufacturing_Lot=='Lot3')$PSI ,mu=1500) #compare means of lot3 with samples
+
+
+
+
